fb0e1ab52d3654c3b2e69a1c3604bec71ff9c176
DenisKP <denis.buserski@gmail.com>
2024-02-09 21:48:53 +0200

    Update Spring MVC project

---
+++ Spring-Boot-MVC/src/main/java/com/example/springbootmvc/web/DenisController.java
@@ -0,0 +1,16 @@
+package com.example.springbootmvc.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/denis")
+public class DenisController {
+
+    @GetMapping("/love")
+    public String denis() {
+        return "denispage";
+    }
+}
--- Spring-Boot-MVC/src/main/java/com/example/springbootmvc/web/HelloController.java
+++ Spring-Boot-MVC/src/main/java/com/example/springbootmvc/web/HelloController.java
@@ -1,7 +1,10 @@
 package com.example.springbootmvc.web;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 @Controller
@@ -17,4 +20,32 @@ public class HelloController {
         modelAndView.setViewName("helloworld");
         return modelAndView;
     }
+
+    @GetMapping("/test1")
+    public String test1(Model model) {
+        model.addAttribute("num", 3);
+        return "testpage";
+    }
+
+    @GetMapping("/test2")
+    public String test2(ModelMap modelMap) {
+        modelMap.put("num", 4);
+        return "testpage";
+    }
+
+    @GetMapping("/test3")
+    public ModelAndView test3(ModelAndView modelAndView) {
+        modelAndView.addObject("num", 5);
+        modelAndView.setViewName("testpage");
+        return modelAndView;
+    }
+
+    @GetMapping("/test4")
+    public String test4(Model model,
+                        @RequestParam("num") Integer num) {
+        model.addAttribute("num", num);
+        return "testpage";
+        // TEST -> http://localhost:8080/test4?num=27
+    }
+
 }
